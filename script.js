778400d8f5fdc6bbf048dcecc9d919ba8d739e87
PrakharMishra31 <prakharmishra3197@gmail.com>
2021-11-28 16:00:40 +0530

    User details

---
+++ script.js
@@ -0,0 +1,26 @@
+const btnGetDetails = document.querySelector(".get_details");
+const table = document.querySelector(".table");
+const tableDetails = document.querySelector(".table_details");
+
+const url = "https://jsonplaceholder.typicode.com/users";
+const getDetails = async function () {
+  try {
+    const resp = await fetch(url);
+    const data = await resp.json();
+    table.classList.remove("hidden");
+    data.forEach((el) => {
+      const detailRow = document.createElement("tr");
+      detailRow.innerHTML = `<td>${el.name}</td>
+      <td>${el.city}</td>
+      <td>${el.email}</td>
+      <td>${el.phone}</td>`;
+      tableDetails.appendChild(detailRow);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+btnGetDetails.addEventListener("click", function () {
+  getDetails();
+});
